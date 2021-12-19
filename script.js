d28fe5338d1ef22c9ee8b3b5ae2cba64d0a09ba0
ulusha <ummelkhatov@gmail.com>
2021-12-19 20:16:50 +0500

    save

---
+++ script.js
@@ -0,0 +1,80 @@
+var num = +prompt('Введите времена суток')
+
+switch(num){
+    case 1:
+        alert('час ночи');
+        break
+    case 2:
+        alert('два часа ночи');
+        break
+    case 3:
+        alert('три часа ночи');
+        break
+    case 4:
+        alert('четыре часа под утро'); 
+        break
+    case 5:
+        alert('пять часов утра')
+        break
+    case 6: 
+        alert('шесть часов утра')
+        break
+    case 7:
+        alert('семь часов утра')
+        break
+    case 8:
+        alert('восемь часов утра')
+        break
+    case 9:
+        alert('девять часов дня')
+        break  
+    case 10:
+        alert('десять часов дня')
+        break  
+    case 11:
+        alert('одинадцать часов дня')
+        break
+    case 12:
+        alert('двенадцать часов дня')
+        break     
+    case 13:
+        alert('час дня')
+        break     
+    case 14:
+        alert('два часа дня')
+        break     
+    case 15:
+        alert('три часа дня')
+        break     
+    case 16:
+        alert('четыре часа дня')
+        break     
+    case 17:
+        alert('пять часов дня')
+        break     
+    case 18:
+        alert('шесть часов вечера')
+        break    
+    case 19:
+        alert('семь часов вечера')
+        break
+    case 20:
+        alert('восемь часов вечера')
+        break   
+    case 21:
+        alert('девять часов вечера')
+        break    
+    case 22:
+        alert('десять часов ночи')
+        break    
+    case 23:
+        alert('одинадцать часов ночи')
+        break    
+    case 24:
+        alert('двенадцать часов ночи')
+        break    
+    default:
+        alert('Вы ввели неправильно')
+        break
+      
+}
